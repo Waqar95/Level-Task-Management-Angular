8eb9cd5cc0e0a95e4d34811800731d925d2026ca
Waqar95 <waqar95ahmed@gmail.com>
2025-06-17 23:30:25 +0200

    Start working with App-header

---
+++ src/app/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    template: './header.Component.html',
+    standalone: true,
+}) 
+
+export class HeaderComponent {}
