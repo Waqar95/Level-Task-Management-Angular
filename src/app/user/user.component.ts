6f7ab7444d96f9f0d7ad3e79427742b213958e2c
waqar95 <waqar95@gmail.com>
2025-07-12 00:00:00 +0200

    image path added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,8 +7,11 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: ['./user.component.css'],
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
 }
